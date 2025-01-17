436e0a7db4f0962d393781786ece911ccdb73bd4
Khalifouille <59296081+Khalifouille@users.noreply.github.com>
2025-01-17 03:37:07 +0100

    fakeid

--- server.lua
+++ server.lua
@@ -45,7 +45,7 @@ RegisterCommand('fairemacarte', function(source, args, rawCommand)
             TriggerClientEvent('esx:showNotification', source, 'Erreur lors de la création de la carte d\'identité.')
         end
     end)
-end, false) 
+end, false)
 
 RegisterCommand('verifiercarte', function(source, args, rawCommand)
     local xPlayer = ESX.GetPlayerFromId(source)
@@ -129,3 +129,51 @@ RegisterCommand('modifinfos', function(source, args, rawCommand)
         end
     end)
 end, false)
+
+RegisterCommand('fakeID', function(source, args, rawCommand)
+    local xPlayer = ESX.GetPlayerFromId(source)
+
+    if not xPlayer then
+        print("Le joueur n'est pas trouvé.")
+        return
+    end
+
+    if #args < 4 then
+        TriggerClientEvent('esx:showNotification', source, 'Usage: /fakeID [Nom] [Prénom] [Âge] [Nationalité]')
+        return
+    end
+
+    local lastname = args[1]
+    local firstname = args[2]
+    local age = tonumber(args[3])
+    local nationality = args[4] or 'Marocaine'
+
+    if not age or age <= 0 then
+        TriggerClientEvent('esx:showNotification', source, 'Veuillez entrer un âge valide.')
+        return
+    end
+
+    local dob = os.date('%Y-%m-%d', os.time() - (age * 365 * 24 * 60 * 60))
+    local query = "INSERT INTO user_identity (identifier, firstname, lastname, dob, nationality, photo, fake_id) VALUES (?, ?, ?, ?, ?, NULL, TRUE)"
+
+    exports.oxmysql:execute(query, {
+        xPlayer.identifier,
+        firstname,
+        lastname,
+        dob,
+        nationality
+    }, function(rowsChanged)
+        if type(rowsChanged) == "table" then
+            if rowsChanged.affectedRows and rowsChanged.affectedRows > 0 then
+                TriggerClientEvent('esx:showNotification', source, 'Votre fausse carte d\'identité a été créée avec succès !')
+            else
+                TriggerClientEvent('esx:showNotification', source, 'Erreur lors de la création de la fausse carte d\'identité.')
+            end
+        elseif type(rowsChanged) == "number" and rowsChanged > 0 then
+            TriggerClientEvent('esx:showNotification', source, 'Votre fausse carte d\'identité a été créée avec succès !')
+        else
+            TriggerClientEvent('esx:showNotification', source, 'Erreur lors de la création de la fausse carte d\'identité.')
+        end
+    end)
+end, false)
+
